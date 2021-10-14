ec9a964bc393c977a93b94fed92f0e73407c3c36
Yegor Shytikov <egorshitiov@gmail.com>
2021-10-13 18:43:07 -0700

    Magento Fork initial commit

--- Controller/Shared/Allcart.php
+++ Controller/Shared/Allcart.php
@@ -3,21 +3,13 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
-
-declare(strict_types=1);
-
 namespace Magento\Wishlist\Controller\Shared;
 
-use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\Context;
-use Magento\Framework\App\Action\HttpPostActionInterface;
 use Magento\Wishlist\Model\ItemCarrier;
 use Magento\Framework\Controller\ResultFactory;
 
-/**
- * Wishlist Allcart Controller
- */
-class Allcart extends Action implements HttpPostActionInterface
+class Allcart extends \Magento\Framework\App\Action\Action
 {
     /**
      * @var WishlistProvider
